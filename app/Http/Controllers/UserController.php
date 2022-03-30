4cfeb47ed8285b27f2d60134b0407509994bd70e
José Marcos <contato.marcosrf@gmail.com>
2022-03-29 23:02:58 -0300

    first commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    public function create(Request $request){
+        
+        if(!empty($request->all())){
+            $user = new User;
+
+            $user->name = $request->name;
+            $user->email = $request->email;
+            $user->password = bcrypt($request->password);
+
+            $user->save();
+
+            dd('Cadastrado com sucesso!');
+        }else{
+            dd('Não foi possivel cadastrar.');
+        }
+
+    }
+}
